b3fe7d701430700a5a5a90ea37c47a43709c91fd
crockct <crockct@mit.edu>
2016-07-18 17:48:49 -0400

    removed sb4 name with periods from tcpdump script

--- bash_tcpdump.sh
+++ bash_tcpdump.sh
@@ -2,7 +2,10 @@
 FILE_PATH=$"/root/GNRS-DOS/tcpdump/"
 
 DATE=`date +%Y-%m-%d_time_%H-%M_`
+
+SB_SUFFIX=$".sb4.orbit-lab.org"
 NODE=`hostname`
+NODE=${NODE%$SB_SUFFIX}
 FILE_EXT=$".pcap"
 
 tcpdump -ttttXvvSw $FILE_PATH$DATE$NODE$FILE_EXT
